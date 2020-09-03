e68d45fc0b40afea0616746ce4b078142a546901
RivaIngunn <ingunn.riva@gmail.com>
2020-09-03 03:53:12 +0200

    work on bias variance plot function, not working

--- frankfunc.py
+++ frankfunc.py
@@ -7,6 +7,7 @@ from random import random, seed
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
 
 class Regression:
     def __init__(self,n,deg):
@@ -55,13 +56,13 @@ class Regression:
         X[:,1] = self.y_[:,0]
         self.X = poly.fit_transform(X)
 
-    def linear_regression(self, ts):
+    def linear_regression(self, ts=0.25):
         # Splitting into train and test data
-        X_train, X_test, f_train, f_test = train_test_split(self.X, self.f, test_size=ts)
+        X_train, X_test, self.f_train, self.f_test = train_test_split(self.X, self.f, test_size=ts)
         # Linear Regression
         
         linreg = LinearRegression()
-        linreg.fit(X_train, f_train)
+        linreg.fit(X_train, self.f_train)
         self.f_predict = linreg.predict(self.X)
         return self.f_predict
 
@@ -79,10 +80,10 @@ class Regression:
         beta = np.linalg.inv(X.T @ X) @ X.T @ y
         return beta
 
-    def linear_regression_homemade(self, ts):
+    def linear_regression_homemade(self, ts=0.25):
         # Splitting into train and test data
-        X_train, X_test, f_train, f_test = train_test_split(self.X, self.f, test_size=ts)
-        self.B = self.betas(X_train, f_train)
+        X_train, X_test, self.f_train, self.f_test = train_test_split(self.X, self.f, test_size=ts)
+        self.B = self.betas(X_train, self.f_train)
         self.f_tilde = self.X @ self.B
         return self.f_tilde
 
@@ -98,19 +99,38 @@ class Regression:
         return self.R2
     
     def bias_variance_plot(self):
+        original_deg = self.deg
+        compl = 100
         x_ = np.linspace(0,1,self.n)
         y_ = np.linspace(0,1,self.n)
+        mse_train = np.zeros(compl)
+        mse_test = np.zeros(compl)
         
-        X = design_matrix_homemade
+        index1 = 0.75*self.n
+        index2 = 0.25*self.n
         
+        for i in range(compl):
+            self.deg = i+1
+            #y_model = self.linear_regression_homemade()
+            #mse_train[i] = self.mean_squared_error(y_model[:75],self.f_train)
+            #mse_test[i] = self.mean_squared_error(y_model[:25],self.f_test)
+            y_model = self.linear_regression()
+            mse_train[i] = mean_squared_error(y_model[:75],self.f_train)
+            mse_test[i] = mean_squared_error(y_model[:25],self.f_test)
         
-        y = self.FrankeFunction(x_, y_)
-        y_pred = 
-        mean_squared_error()
+        complexity = np.linspace(1,compl,compl)
+        plt.plot(complexity , mse_train, label = 'train error')
+        plt.plot(complexity , mse_test,label = 'test error')
+        plt.xlabel('complexity')
+        plt.ylabel('Prediction Error')
+        plt.legend()
+        plt.show()
+        self.deg = original_deg
         
 
 reg = Regression(100,2)
 reg.dataset2D()
 reg.design_matrix_homemade()
 f_pred = reg.linear_regression_homemade(0.2)
+reg.bias_variance_plot()
 ##

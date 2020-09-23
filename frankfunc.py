38f01a3cffb0b038180db9d7e95f9543e3acc8c2
RivaIngunn <ingunn.riva@gmail.com>
2020-09-23 08:53:30 +0200

    added kfold and ridge again, kfold should be working

--- frankfunc.py
+++ frankfunc.py
@@ -51,6 +51,11 @@ class Regression():
         self.X_train, self.X_test, self.f_train, self.f_test = train_test_split(X,f,test_size=0.2)
         return self.X_train, self.X_test, self.f_train, self.f_test
 
+
+    def MSE(self,y,y_pred):
+        mse = np.mean((y - y_pred)**2)
+        return mse
+    
     def OLS(self, X_train, X_test, f_train):
         beta = np.linalg.pinv(X_train.T @ X_train) @ X_train.T @ f_train
 
@@ -62,11 +67,99 @@ class Regression():
     def bootstrap(self, X_test, X_train, f_train, trials):
         z_pred = np.zeros((self.f_test.shape[0], trials))
         for i in range(trials):
-            X_train, f_train = resample(X_train, f_train)
-            z_pred[:,i] = self.OLS(X_train, X_test, f_train)[1]
+            X_new, f_new = resample(X_train, f_train)
+            z_pred[:,i] = self.OLS(X_new, X_test, f_new)[1]
         return z_pred
+    
+    def ridge(self, X_train, X_test, f_train, lam):
+        beta = np.linalg.pinv(X_train.T @ X_train + np.identity(len(X_train[0,:]))*lam) @ X_train.T @ f_train
+        f_tilde = X_train @ beta
+        f_pred = X_test @ beta
+        
+        return f_tilde, f_pred
+    
+    def k_fold(self,X,k,deg):
+        # X = np.random.shuffle(X_train)
+        
+        #scaling data
+        X[:,1:] -= np.mean(X[:,1:], axis=0)
+        f = self.f - np.mean(self.f)
+        
+        #splitting data
+        X_lst1 = np.split(X,k)
+        f_lst1 = np.split(f,k)
+        
+        X_lst = []
+        f_lst = []
+        
+        mse_score = np.zeros(k)
+        bias = np.zeros(k)
+        variance = np.zeros(k)
+        test_err_arr = np.zeros(k)
+        train_err_arr = np.zeros(k)
 
-n = 400; maxdeg = 10
+        #getting rid of nested list
+        for i in range(len(X_lst1)):
+            X_lst.append(X_lst1[i])
+            f_lst.append(f_lst1[i])
+            
+        X_lst = np.array(X_lst)
+        f_lst = np.array(f_lst)
+        
+        for i in range(len(X_lst)):
+            
+            X_train = np.concatenate([X_lst[:i],X_lst[i+1:]])
+            f_train = np.concatenate([f_lst[:i],f_lst[i+1:]])
+            X_test = X_lst[i]
+            f_test = f_lst[i]
+
+                        
+            f_train_lst = []
+            X_train_lst = []
+            
+            # do this in the other loop?
+            for k in range(len(X_train)):
+                for j in range(len(X_train[k])):
+                    f_train_lst.append(f_train[k][j])
+                    X_train_lst.append(X_train[k][j])
+                
+            f_train = np.array(f_train_lst)
+            X_train = np.array(X_train_lst)
+            
+            f_tilde, z_pred = self.OLS(X_train,X_test,f_train)
+
+            
+            # mse_score[i] = self.mse(f_test,f_pred)
+            mse_score[i] = self.MSE(f_test,z_pred)
+            
+            
+            # bias[i] = np.mean((f_test - np.mean(z_pred))**2)
+            average_model = np.mean(z_pred)
+            bias[i] = np.mean((average_model - f_test)**2)
+        
+            
+            variance[i] = np.mean(np.var(z_pred, axis=0))
+        
+        
+            test_err_arr[i] = np.mean( (z_pred - f_test)**2 )
+            train_err_arr[i] = np.mean( (f_tilde - f_train)**2 )
+
+            
+          
+            
+        error = np.mean(mse_score)
+        
+        # print ('variance', np.mean(variance))
+        # print ('test_error', np.mean(test_err_arr))
+        # print ('train_error',np.mean(train_err_arr))
+        # print ('bias', np.mean(bias))
+        
+        # print ('deg',deg,'score',error,'\n')
+        
+        return np.mean(bias), np.mean(variance), error , np.mean(test_err_arr), np.mean(train_err_arr)
+
+
+n = 200; maxdeg = 10
 # Arrays for plotting error
 degrees = np.linspace(1,maxdeg,maxdeg)
 train_error = np.zeros(maxdeg)
@@ -107,7 +200,9 @@ for i in range(maxdeg):
 
     bias2[i] = np.mean( (reg.f_test - np.mean(f_hat))**2 )
     variance2[i] = np.mean(np.var(f_strap, axis=1, keepdims=True))
-
+    
+    reg.k_fold(reg.X,5,deg)
+    
 plt.plot(degrees, train_error, label='Training Error')
 plt.plot(degrees, test_error, label='Test Error')
 plt.legend()

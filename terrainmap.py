5e2a1e85d93cb44ff51d3dbbdc257d86c91fb09e
orjanstr <orjanstr@student.matnat.uio.no>
2020-09-28 11:16:26 +0200

    organized data in terrainmap.py

--- terrainmap.py
+++ terrainmap.py
@@ -11,8 +11,14 @@ resolution = 30 # Meters (according to website)
 y_dim, x_dim = len(terrain1[:,0]), len(terrain1[0])
 
 # Setting up x and y arrays
-x = np.linspace(0, x_dim, x_dim-1)*resolution
-y = np.linspace(0, y_dim, y_dim-1)*resolution
+x = np.linspace(0, x_dim-1, x_dim)*resolution
+y = np.linspace(0, y_dim-1, y_dim)*resolution
+x, y = np.meshgrid(x,y)
+
+# Raveling nata to get into shape (x_dim*y_dim,)
+z = terrain1.ravel()
+x = x.ravel()
+y = y.ravel()
 
 plt.figure()
 plt.title('Terrain over Norway')
